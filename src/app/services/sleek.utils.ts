e6bab1b63c6947933585ec9db99c6bbaab90a32c
Marius Guede <marius.guede@sleeksys.com>
2025-03-28 11:06:22 +0100

    Templates: add helper SleekUtils

---
+++ src/app/services/sleek.utils.ts
@@ -0,0 +1,14 @@
+export class SleekUtils {
+
+  static generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      const r = Math.random() * 16 | 0;
+      const v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
+  static generateHash() {
+    return this.generateUUID().substring(0, 8);
+  }
+}
